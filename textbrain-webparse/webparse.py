28f83276284750425d73d44148d734d705490220
Christian Henkel <post@henkelchristian.de>
2015-12-21 07:05:05 +0000

    now with a rest interface

--- textbrain-webparse/webparse.py
+++ textbrain-webparse/webparse.py
@@ -5,10 +5,12 @@ import nltk
 import thread
 import flask
 import time
+import json
 app = flask.Flask(__name__)
 
 class webparse:
-    def __init__(self, pw):
+    def __init__(self):
+        pw = raw_input("Choose a passphrase ...")
         # variables
         self.pw = pw
         self.queue = []
@@ -21,13 +23,38 @@ class webparse:
         print "started"
         #t1.join()
         
+    def add_url(self, url):
+        if url not in self.queue:
+            self.queue.append(url)
+            
+    def check_pw(self, pw_):
+        return (pw_ == self.pw)
+        
+    def work(self):
+        if self.queue.__len__() == 0:
+            print "Queue empty"
+        else:
+            start = time.time()
+            url = self.queue.pop(0)
+            text = self.from_url(url, "lxml")
+            end = time.time()
+            print "Parsing of " + url + " took " + str(end - start)
         
     def from_url(self, url, parser):
         # Source: http://stackoverflow.com/a/24618186
         
-        html = urllib2.urlopen(url).read()
-        soup = bs4.BeautifulSoup(html, parser)
-        
+        try:
+            html = urllib2.urlopen(url).read()
+        except Exception, e:
+            print "ERROR: " + str(e)
+            print "failed to open: >" + url + "<"
+            return ""
+            
+        try:
+            soup = bs4.BeautifulSoup(html, parser)
+        except Exception, e:
+            print "ERROR: " + str(e)
+         
         # kill all script and style elements
         for script in soup(["script", "style"]):
             script.extract()    # rip it out
@@ -44,13 +71,23 @@ class webparse:
         
         return text
 
-pw = raw_input("Choose a passphrase ...")
-wp = webparse(pw)
+wp = webparse()
 
-@app.route('/')
-def hello_world():
-    global wp
-    return wp.from_url("http://www.zeit.de/politik/ausland/2015-12/parlamentswahlen-spanien-podemos-mariano-rajoy", "lxml")
+@app.route('/api/<pw>', methods=['POST'])
+def api(pw):
+    print "api call with >" + pw + "<"
+    if not wp.check_pw(pw):
+        print "wrong pw"
+        return ("wrong pw", 401)
+    else:
+        print "pw ok"
+        try:
+            urls = json.loads(flask.request.data)
+            for url in urls:
+                wp.add_url(url)
+        except Exception, e:
+            return("Exception: " + e, 404) 
+        return ("ok", 200)
     
 if __name__ == "__main__":
     global wp
@@ -60,4 +97,5 @@ if __name__ == "__main__":
     # print wp.from_url("http://www.google.com", "lxml")
     
     while (True):
-        time.sleep(1)
+        time.sleep(2)
+        wp.work()
